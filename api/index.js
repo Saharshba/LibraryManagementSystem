137fa6c6e002a4ac107eb50bb37e30c20cea6ce9
Saharshba <saharshba4@gmail.com>
2026-05-26 11:54:12 +0530

    Apply Vercel root api entrypoint and function limits

--- api/index.js
+++ api/index.js
@@ -1,45 +1,13 @@
 const serverless = require('serverless-http');
-const app = require('../server/src/app');
 const connectDB = require('../server/src/config/db');
-const seedAdmin = require('../server/src/utils/seedAdmin');
+const app = require('../server/src/app');
 
-let initializationPromise = null;
-
-const withTimeout = async (promise, timeoutMs, label) => {
-  let timeoutId;
-
-  const timeoutPromise = new Promise((_, reject) => {
-    timeoutId = setTimeout(() => {
-      reject(new Error(`${label} timed out after ${timeoutMs}ms`));
-    }, timeoutMs);
-  });
-
-  try {
-    return await Promise.race([promise, timeoutPromise]);
-  } finally {
-    clearTimeout(timeoutId);
-  }
-};
-
-const ensureReady = async () => {
-  if (!initializationPromise) {
-    initializationPromise = (async () => {
-      try {
-        await withTimeout(connectDB(), 10000, 'MongoDB connection');
-        await withTimeout(seedAdmin(), 10000, 'Admin seed');
-      } catch (error) {
-        initializationPromise = null;
-        throw error;
-      }
-    })();
-  }
-
-  return initializationPromise;
-};
-
-const handler = serverless(app);
+let isConnected = false;
 
 module.exports = async (req, res) => {
-  await ensureReady();
-  return handler(req, res);
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+  return serverless(app)(req, res);
 };
